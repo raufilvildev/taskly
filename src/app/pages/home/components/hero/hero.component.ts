fd180f6f0a59b0636bb8895d88dd29b210621c06
Ireneders <ireneders@gmail.com>
2025-06-05 19:01:56 +0200

    Instalo gsap
    Instalo gsap y pruebo animaciones en la home

--- src/app/pages/home/components/hero/hero.component.ts
+++ src/app/pages/home/components/hero/hero.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent{}
