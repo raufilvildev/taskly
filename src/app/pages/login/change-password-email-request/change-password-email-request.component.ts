f12759dc7a1726e7fb340c353d1229537feb2c4c
Pablopserrada <pablopserrada@gmail.com>
2025-06-10 21:01:55 +0200

    Cambios en el change password sin terminar

--- src/app/pages/login/change-password-email-request/change-password-email-request.component.ts
+++ src/app/pages/login/change-password-email-request/change-password-email-request.component.ts
@@ -1,14 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { AuthorizationService } from '../../../services/authorization.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-change-password-email-request',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './change-password-email-request.component.html',
   styleUrl: './change-password-email-request.component.css',
 })
 export class ChangePasswordEmailRequestComponent {
   authorizationService = inject(AuthorizationService)
+  router = inject(Router)
 
-  
+  async checkEmail(email:any) {
+    try {
+      let response = await this.authorizationService.checkEmail(email);
+      if (response.token) {
+        this.authorizationService.requestConfirmationByEmail(response.token, 'change_password')
+        this.router.navigate(['/login/change_password'])
+      }
+    } catch (msg:any) {
+      alert('Email incorrecto')
+    }
+  }
 }
