77f6d39c4f4aae8203fbcf6dbcd4a4e64d8f6aeb
Lucho <lsepu19@gmail.com>
2025-06-13 22:17:02 +0200

    Cambio componentes a shared + agrego filtros
    Cambio los componentes del dashboard_task que se pueden reutilizar en otros lugares, también pongo filtros un poco mas "fijos" para organizarlo mejor

--- src/app/pages/dashboard/dashboard-list-view/dashboard-list-view.component.ts
+++ src/app/pages/dashboard/dashboard-list-view/dashboard-list-view.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { AngularSplitModule } from 'angular-split';
 import { ListGroupsComponent } from "./components/list-groups/list-groups.component";
-import { TaskListComponent } from "./components/task-list/task-list.component";
-import { TaskDetailComponent } from "./components/task-detail/task-detail.component";
+import { TaskListComponent } from "../../../shared/components/task-list/task-list.component";
+import { TaskDetailComponent } from "../../../shared/components/task-detail/task-detail.component";
+import { ITask } from '../../../interfaces/itask';
 
 @Component({
   selector: 'app-dashboard-list-view',
@@ -11,5 +12,5 @@ import { TaskDetailComponent } from "./components/task-detail/task-detail.compon
   styleUrl: './dashboard-list-view.component.css'
 })
 export class DashboardListViewComponent {
-
+  tasks: ITask[] = [];
 }
