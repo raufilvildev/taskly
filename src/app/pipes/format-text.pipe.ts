27bcbbe09dc6ec3b31f7fbdb164a918dda6c1dcf
Raúl Filigrana Villalba <raufilvil@gmail.com>
2025-06-13 12:19:47 +0200

    Corrijo errata en format-text.pipe

--- src/app/pipes/format-text.pipe.ts
+++ src/app/pipes/format-text.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatTextPipe implements PipeTransform {
   transform(value: string): string {
-    return value.replace(/\\n/g, '\n\n');
+    return value.replace(/\\n/g, '\n');
   }
 }
