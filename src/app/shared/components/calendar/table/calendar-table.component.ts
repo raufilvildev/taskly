ade5596b056a04089c434833c237ec8c712a3686
Dru <dev.morera@gmail.com>
2025-06-18 18:47:26 +0200

    Update dashboard-calendar to use app-calendar-table component, replacing app-table. Modify calendar-table component to change content and selector for consistency.

--- src/app/shared/components/calendar/table/calendar-table.component.ts
+++ src/app/shared/components/calendar/table/calendar-table.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-table',
+  selector: 'app-calendar-table',
   imports: [],
   templateUrl: './calendar-table.component.html',
   styleUrl: './calendar-table.component.css'
