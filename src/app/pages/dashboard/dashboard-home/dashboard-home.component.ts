516f16641d8e050a5b7dd44c1a67412afc90e66c
Ireneders <ireneders@gmail.com>
2025-06-23 18:16:47 +0200

    Empiezo dashboard home

--- src/app/pages/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/pages/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { TableComponent } from "../../../shared/components/calendar/table/calendar-table.component";
+import { TasksComponent } from "../../../shared/components/tasks/tasks.component";
+import { TaskListComponent } from '../../../shared/components/task-list/task-list.component';
+import { ITask } from '../../../interfaces/itask';
+import { RouterLink } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-dashboard-home',
-  imports: [],
+  imports: [TableComponent, TaskListComponent, RouterLink],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.css'
 })
 export class DashboardHomeComponent {
+  tasks: ITask[] = [];
 
 }
