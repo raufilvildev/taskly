98ce775083289cb2cb255d94d73950b59beb2a8b
Ireneders <ireneders@gmail.com>
2025-06-24 19:54:57 +0200

    Cambios dashboard home

--- src/app/pages/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/pages/dashboard/dashboard-home/dashboard-home.component.ts
@@ -4,12 +4,15 @@ import { TasksComponent } from "../../../shared/components/tasks/tasks.component
 import { TaskListComponent } from '../../../shared/components/task-list/task-list.component';
 import { ITask } from '../../../interfaces/itask';
 import { RouterLink } from '@angular/router';
+import { DashboardHomeCalendarComponent } from './components/dashboard-home-calendar/dashboard-home-calendar.component';
+import { UsersService } from '../../../services/users.service';
 
 
 
 @Component({
   selector: 'app-dashboard-home',
-  imports: [TableComponent, TaskListComponent, RouterLink],
+  standalone: true,
+  imports: [TaskListComponent, RouterLink, DashboardHomeCalendarComponent],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.css'
 })
