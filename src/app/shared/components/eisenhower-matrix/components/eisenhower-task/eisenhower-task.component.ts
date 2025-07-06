6e431b108c8e00bf7d6f9a95d13c89c0dacbcb45
Dru <dev.morera@gmail.com>
2025-07-06 14:50:34 +0200

    Implement task completion toggle functionality in Eisenhower Matrix component
    - Added a checkbox to toggle the completion status of tasks.
    - Updated task title and description to visually indicate completion with a line-through style.
    - Integrated a service call to update the task status in the backend upon toggling.

--- src/app/shared/components/eisenhower-matrix/components/eisenhower-task/eisenhower-task.component.ts
+++ src/app/shared/components/eisenhower-matrix/components/eisenhower-task/eisenhower-task.component.ts
@@ -1,14 +1,45 @@
 import { Component, Input } from '@angular/core';
 import { ITask } from '../../../../../interfaces/itask.interface';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
+import { TasksService } from '../../../../../services/tasks.service';
 
 @Component({
   selector: 'app-eisenhower-task',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DatePipe],
   templateUrl: './eisenhower-task.component.html',
-  styleUrl: './eisenhower-task.component.css',
+  styleUrls: ['./eisenhower-task.component.css']
 })
 export class EisenhowerTaskComponent {
-  @Input({ required: true }) task!: ITask;
+  @Input() task!: ITask;
+
+  constructor(private tasksService: TasksService) {}
+
+  toggleCompletion(event: Event) {
+    event.stopPropagation();
+    const updateData: Partial<ITask> = {
+      is_completed: !this.task.is_completed,
+      title: this.task.title,
+      description: this.task.description,
+      due_date: this.task.due_date,
+      is_urgent: this.task.is_urgent,
+      is_important: this.task.is_important,
+      subtasks: this.task.subtasks,
+      category: this.task.category,
+      time_start: this.task.time_start,
+      time_end: this.task.time_end
+    };
+
+    this.tasksService.updateTask(this.task.uuid, updateData).subscribe({
+      next: (updatedTask) => {
+        // Actualizar el estado local
+        this.tasksService.tasks.update(tasks =>
+          tasks.map(t => t.id === updatedTask.id ? updatedTask : t)
+        );
+      },
+      error: (error) => {
+        console.error('Error al actualizar la tarea:', error);
+      }
+    });
+  }
 }
